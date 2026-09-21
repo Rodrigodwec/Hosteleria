a770bab4cc5ed629df5f158ecf179e40499c7838
Rodrigodwec <rdelgadob24@fpcoruna.afundacion.org>
2026-09-21 14:36:49 +0200

    usuario y mesa model

--- frontendpractica/src/app/core/models/mesa.model.ts
+++ frontendpractica/src/app/core/models/mesa.model.ts
@@ -4,7 +4,7 @@ export interface Mesa {
     id: number;
     numero: number;
     capacidad: number;
-    estado: String;
-    camareroId: number;
-    camareroNombre: String;
+    estado: EstadoMesa;
+    camareroId: number | null;
+    camareroNombre: string | null;
 }
